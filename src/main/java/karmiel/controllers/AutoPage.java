02daca485da91c1e08b8c939759aa1c2978cfb70
xcplaza <xcplaza@gmail.com>
2023-12-01 16:10:06 +0200

    add class edit car

--- src/main/java/karmiel/controllers/AutoPage.java
+++ src/main/java/karmiel/controllers/AutoPage.java
@@ -6,6 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class AutoPage {
@@ -18,5 +24,37 @@ public class AutoPage {
         Iterable<Auto> autos = autoRepo.findAll();
         model.addAttribute("autos", autos);
         return "auto";
+    }
+
+    @GetMapping("/auto/add")
+    public String add(Model model) {
+        model.addAttribute("title", "Добавление автомобиля");
+        return "auto-add";
+    }
+
+    @PostMapping("/auto/add")
+    public String addAuto(
+            @RequestParam String name,
+            @RequestParam String model,
+            @RequestParam String year,
+            @RequestParam String color,
+            @RequestParam String price,
+            Model models) {
+        Auto auto = new Auto(name, model, year, color, price);
+        autoRepo.save(auto);
+        return "redirect:/auto";
+    }
+
+    @GetMapping("/auto/{id}")
+    public String autoDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!autoRepo.existsById(id)) {
+            return "redirect:/auto";
+        }
+        Optional<Auto> auto = autoRepo.findById(id);
+        ArrayList<Auto> res = new ArrayList<>();
+        auto.ifPresent(res::add);
+        model.addAttribute("auto", res);
+        return "auto-details";
+
     }
 }
--- src/main/java/karmiel/entity/Auto.java
+++ src/main/java/karmiel/entity/Auto.java
@@ -19,4 +19,14 @@ public class Auto {
     private String price;
     private int view;
 
+    public Auto() {
+    }
+
+    public Auto(final String name, final String model, final String year, final String color, final String price) {
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+    }
 }
