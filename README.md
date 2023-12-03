014e802cb659e8654bff200119620ba015ecf3ea
xcplaza <xcplaza@gmail.com>
2023-12-03 13:02:55 +0200

    correct auto objectand README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project presales auto
 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJHJlRA94QJ0CicbhZSBYwJkZzusRuwmv7yQ&usqp=CAU"/>
+
+Install DB - import db(spring_auto.sql) to local MySql server. 
--- src/main/java/karmiel/entity/Auto.java
+++ src/main/java/karmiel/entity/Auto.java
@@ -1,13 +1,15 @@
 package karmiel.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Auto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -18,9 +20,6 @@ public class Auto {
     private String color;
     private String price;
     private int view;
-
-    public Auto() {
-    }
 
     public Auto(final String name, final String model, final String year, final String color, final String price) {
         this.name = name;
