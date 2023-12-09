1717379930c6d69da4231e4f389cd9cc063c4593
xcplaza <xcplaza@gmail.com>
2023-12-09 23:09:37 +0200

    add module CarApprove

---
+++ CarApprove/src/main/java/karmiel/CarApproveApp.java
@@ -0,0 +1,12 @@
+package karmiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarApproveApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarApproveApp.class, args);
+    }
+}
