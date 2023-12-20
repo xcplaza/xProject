87bcbc7f252ed0ecd3e491d1dfa04811655e6a9a
svyat <svyatoslav.irishin@gmail.com>
2023-12-20 17:38:34 +0200

    CarPrepairService makes 2 objects: CarReadyDTO and CarNotReadyDTO ver 1.3

--- CarApprove/src/main/java/karmiel/service/CarProcessingService.java
+++ CarApprove/src/main/java/karmiel/service/CarProcessingService.java
@@ -1,76 +1,52 @@
 package karmiel.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import karmiel.dto.CarAprDTO;
-import karmiel.dto.CarDTO;
-import karmiel.dto.CarRejDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import karmiel.kafka.KafkaProducer;
 
 @Service
+@EnableBinding(CarProcessingService.CarProcessor.class)
 public class CarProcessingService {
-    private final KafkaProducer kafkaProducer;
-    private final ObjectMapper objectMapper;
 
     @Autowired
-    public CarProcessingService(KafkaProducer kafkaProducer, ObjectMapper objectMapper) {
-        this.kafkaProducer = kafkaProducer;
-        this.objectMapper = objectMapper;
-    }
+    private CarProcessor processor;
 
-    public void processCar(String carDTOAsString) {
+    @StreamListener(CarProcessor.INPUT)
+    public void processCar(CarDTO carDTO) {
         try {
-            // Преобразование строки в объект CarDTO
-            CarDTO carDTO = objectMapper.readValue(carDTOAsString, CarDTO.class);
-
-            if ((carDTO.isBumper() || carDTO.isWindscreen() || carDTO.isClean())
-                    && (carDTO.getState() < 50 || (carDTO.getState() < 60 && !carDTO.isBumper() && !carDTO.isWindscreen() && !carDTO.isClean()))) {
-                // Отклонение автомобиля
-                CarRejDTO carRejDTO = createRejectionDTO(carDTO);
-                kafkaProducer.sendRejectionMessage(carRejDTO);
+            if (carDTO.getState() > 50) {
+                processor.outputApproval().send(MessageBuilder.withPayload(convertToCarAprDTO(carDTO)).build());
             } else {
-                // Утверждение автомобиля
-                CarAprDTO carAprDTO = createApprovalDTO(carDTO);
-                kafkaProducer.sendApprovalMessage(carAprDTO);
+                processor.outputRejection().send(MessageBuilder.withPayload(convertToCarRejDTO(carDTO)).build());
             }
         } catch (Exception e) {
-            // Обработка ошибок, если не удалось отправить сообщение в Kafka
             e.printStackTrace();
         }
     }
 
-    private CarRejDTO createRejectionDTO(CarDTO carDTO) {
-        return new CarRejDTO(
-                carDTO.getNumber(),
-                carDTO.getVin(),
-                carDTO.getBrand(),
-                carDTO.getModel(),
-                carDTO.isBumper(),
-                carDTO.isWindscreen(),
-                carDTO.isClean(),
-                carDTO.getState(),
-                carDTO.getColor(),
-                carDTO.getYears(),
-                carDTO.getMileage(),
-                carDTO.getPrice()
+    private CarAprDTO convertToCarAprDTO(CarDTO carDTO) {
+        return new CarAprDTO(
+                carDTO.getNumber(), carDTO.getVin(), carDTO.getBrand(), carDTO.getModel(),
+                carDTO.isBumper(), carDTO.isWindscreen(), carDTO.isClean(), carDTO.getState(),
+                carDTO.getColor(), carDTO.getYears(), carDTO.getMileage(), carDTO.getPrice()
         );
     }
 
-    private CarAprDTO createApprovalDTO(CarDTO carDTO) {
-        return new CarAprDTO(
-                carDTO.getNumber(),
-                carDTO.getVin(),
-                carDTO.getBrand(),
-                carDTO.getModel(),
-                carDTO.isBumper(),
-                carDTO.isWindscreen(),
-                carDTO.isClean(),
-                carDTO.getState(),
-                carDTO.getColor(),
-                carDTO.getYears(),
-                carDTO.getMileage(),
-                carDTO.getPrice()
+    private CarRejDTO convertToCarRejDTO(CarDTO carDTO) {
+        return new CarRejDTO(
+                carDTO.getNumber(), carDTO.getVin(), carDTO.getBrand(), carDTO.getModel(),
+                carDTO.isBumper(), carDTO.isWindscreen(), carDTO.isClean(), carDTO.getState(),
+                carDTO.getColor(), carDTO.getYears(), carDTO.getMileage(), carDTO.getPrice()
         );
+    }
+
+    interface CarProcessor {
+        @Input
+        SubscribableChannel input();
+
+        @Output
+        MessageChannel outputApproval();
+
+        @Output
+        MessageChannel outputRejection();
     }
 }
